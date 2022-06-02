12a3d0f26bc8a31f225e2efb8c9b1072dfad5138
Nathan Yi <nathanyi112@gmail.com>
2022-06-01 22:50:09 -0500

    adds index controller for router

--- controllers/EntryCtrl/Index.js
+++ controllers/EntryCtrl/Index.js
@@ -1,7 +1,19 @@
 ////////////////////////
 // Setup - Import deps
 ////////////////////////
-
+const Entry = require("../models/Entry");
 ///////////////////////
 // Exports
 ///////////////////////
+module.exports = {
+  index,
+};
+
+async function index(req, res) {
+  try {
+    let entries = await Entry.find({});
+    res.json(entries);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+}
